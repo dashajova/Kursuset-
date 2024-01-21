29c01200a6845f855285da5766b03779305b341b
DARJA <vorobjova.darja12@gmail.com>
2024-01-21 19:27:19 +0200

    Initial commit

--- express_server/public/server.js
+++ express_server/public/server.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
-
-// Middleware for handling JSON requests
-app.use(express.json());
-
-// Your routes and logic will go here
-
+const port = 5500;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/views/index.html/');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/about.html');
+  res.sendFile(__dirname + '/public/views/about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(__dirname + '/public/contact.html');
+  res.sendFile(__dirname + '/public/views/contact.html');
 });
+
+app.get('/profile', (req, res) => {
+  res.sendFile(__dirname + '/public/views/profile.html');
+});
+app.get('/profile', (req, res) => {
+  res.sendFile(__dirname + '/public/views/home.html');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
